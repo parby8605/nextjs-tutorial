62f1134b5f1011f8d0609d8de27a1e6f359f1cac
HeeJeong Lee <lhj971011@gmail.com>
2024-05-27 14:44:42 +0900

    api-post-params설정

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,12 +1,18 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request, { params }) {
+export async function POST(request, { params }) {
   const { id } = params;
+  const { address, age } = await request.json();
+  //   const { searchParams } = new URL(request.url);
+  //   console.log(searchParams.get("address"));
+  //   console.log(searchParams.get("age"));
+  console.log("전달받은 params : ", address, age);
 
   if (id === "123") {
     return NextResponse.json({
       name: "Aaron",
-      age: 10,
+      ...(age === "true" ? { age: 10 } : {}),
+      ...(address === "true" ? { address: "경기도 고양시" } : {}),
     });
   }
   return new NextResponse();
--- src/app/dashboard/(easywork)/customers/[nickname]/page.js
+++ src/app/dashboard/(easywork)/customers/[nickname]/page.js
@@ -1,7 +1,13 @@
 export default async function NicknameCustomer({ params, searchParams }) {
   const { nickname } = params;
 
-  const response = await fetch("http://localhost:3000/api/users/123");
+  const response = await fetch("http://localhost:3000/api/users/123", {
+    method: "POST",
+    body: JSON.stringify({
+      address: searchParams.address,
+      age: searchParams.age,
+    }),
+  });
   const data = await response.json();
 
   console.log(data);
@@ -9,6 +15,7 @@ export default async function NicknameCustomer({ params, searchParams }) {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       어떤 유저의 정보를 표시할까요? <b>{nickname}</b>
+      <div>{JSON.stringify(data)}</div>
     </main>
   );
 }
