5391d4af4258cf9748a690337638663d8ffe7219
HeeJeong Lee <lhj971011@gmail.com>
2024-05-27 10:03:24 +0900

    routing tutorial

---
+++ src/app/dashboard/customers/page.jsx
@@ -0,0 +1,7 @@
+export default function Customers() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="text-blue-300">Hello Dashboard</div>
+    </main>
+  );
+}
---
+++ src/app/dashboard/invoices/page.jsx
@@ -0,0 +1,7 @@
+export default function Invoices() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="text-green-500">Hello Invoices</div>
+    </main>
+  );
+}
