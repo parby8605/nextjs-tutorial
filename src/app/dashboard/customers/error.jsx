32b2e0fba52be9f2a50800933c8213b57116348e
HeeJeong Lee <lhj971011@gmail.com>
2024-05-27 10:07:41 +0900

    error page - use client

---
+++ src/app/dashboard/customers/error.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function CustomersError() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2>에러가 발생</h2>
+    </main>
+  );
+}
--- src/app/dashboard/customers/page.jsx
+++ src/app/dashboard/customers/page.jsx
@@ -1,4 +1,7 @@
 export default function Customers() {
+  if (true) {
+    throw new Error("일부러 발생시킨 에러");
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-blue-300">Hello Dashboard</div>
